6d95f36fe38a5c6ab06ac1606bc6dec6881b02d8
Shubham Dangwal <shdangwal@gmail.com>
2024-11-29 23:22:21 +0530

    test user.service.ts

---
+++ src/app/shared/services/users.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { UserService } from './users.service';
+import { UserInterface } from '../types/user.interface';
+
+describe('UserService', () => {
+  let userService: UserService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UserService],
+    });
+    userService = TestBed.inject(UserService);
+  });
+
+  it('creates a service', () => {
+    expect(userService).toBeTruthy();
+  });
+
+  describe('addUser', () => {
+    it('should add user', () => {
+      const user: UserInterface = {
+        id: '3',
+        name: 'foo',
+      };
+      userService.addUser(user);
+      expect(userService.users).toEqual([{ id: '3', name: 'foo' }]);
+    });
+  });
+
+  describe('removeUser', () => {
+    it('should remove user', () => {
+      userService.users = [{ id: '3', name: 'foo' }];
+      userService.removeUser('3');
+      expect(userService.users).toEqual([]);
+    });
+  });
+});
