b00f3fa34b85310c4f3dbc59db3bd9982d5223f4
Shubham Dangwal <shdangwal@gmail.com>
2024-12-05 10:49:02 +0530

    api.service.ts testing

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { TagInterface } from '../types/tag.interface';
 
 @Injectable()
 export class ApiService {
